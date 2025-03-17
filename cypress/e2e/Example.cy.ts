2b3fe373e676d2f4e16ced98a53376d53e1e231f
VitmiTor <torremvictor@gmail.com>
2025-03-16 21:34:27 -0500

    first cypress

---
+++ cypress/e2e/Example.cy.ts
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.saucedemo.com/v1/')
+
+    cy.get('[data-test="username"]').type("standard_user")
+    cy.get('[data-test="password"]').type("secret_sauce")
+    cy.get('#login-button').click()
+
+    cy.get('.product_label').should('exist').contains('Products')
+  })
+})
